d6528c16f6d27ce58ac4da55db06efef41e09a29
jadewunm <112850624+jadewunm@users.noreply.github.com>
2024-06-09 05:22:04 +0000

    Initial commit

---
+++ FeetMetersConversion.java
@@ -0,0 +1,27 @@
+public class FeetMetersConversion {
+    /** Convert from feet to meters */
+    public static double footToMeter(double foot) {
+        return 0.305 * foot;
+    }
+
+    /** Convert from meters to feet */
+    public static double meterToFoot(double meter) {
+        return 3.279 * meter;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Feet\tMeters");
+        System.out.println("----\t------");
+        for (double feet = 1.0; feet <= 10.0; feet++) {
+            double meters = footToMeter(feet);
+            System.out.printf("%.1f\t%.3f\n", feet, meters);
+        }
+
+        System.out.println("\nMeters\tFeet");
+        System.out.println("------\t----");
+        for (double meters = 20.0; meters <= 65.0; meters += 5.0) {
+            double feet = meterToFoot(meters);
+            System.out.printf("%.1f\t%.3f\n", meters, feet);
+        } // Added closing brace for main method
+    }
+}
